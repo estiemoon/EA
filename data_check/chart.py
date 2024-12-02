2d754fb6d4d69e336c3e8f893f0e113fd58c5ea4
estie <harin0375@gmail.com>
2024-12-02 14:12:09 +0900

    변수 차트 확인

---
+++ data_check/chart.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_excel("../results/ecm_result4.4.xlsx")
+x_headers = df.columns.to_list()
+
+x = st.selectbox(
+    "CHOOSE",
+    x_headers,
+)
+x_vals = df[x]
+x_vals = x_vals.dropna() 
+
+st.write("You selected:", x)
+b = st.slider("bins", 0, 100, 40, 1)
+
+min_v = st.slider("min_x", int(min(x_vals)), 20, int(min(x_vals)), 1)
+max_v = st.slider("max_x", 0, int(max(x_vals)), int(max(x_vals)), 1)
+
+
+fig, ax = plt.subplots()
+ax.hist(x_vals, bins=b, range=(min_v,max_v))
+
+st.pyplot(fig)
